ac725c77a74b0bf680a704125b9701f394e4ba96
Jon Ambas <jon@jonambas.com>
2017-09-04 19:52:11 -0400

    Basic setup

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'index.js',
+    format: 'cjs'
+  },
+  plugins: [
+    resolve(),
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      presets: [
+        [
+          'es2015',
+          { modules: false }
+        ],
+        'stage-2',
+        'react'
+      ],
+      plugins: [
+        'external-helpers'
+      ]
+    }),
+    uglify()
+  ],
+  external: [
+    'react',
+    'react-dom',
+    'prop-types'
+  ],
+};
